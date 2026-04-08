13fd881c4d59f789a5fb26b102e8fb28cf2cebc9
mjyuung <yoonminjoo03@ewhain.net>
2026-04-09 00:04:28 +0900

    Add: Player_Interaction
    - 문 열리고 닫히는 애니메이션 추가
    - 플레이어 레이캐스팅 추가

---
+++ Assets/Scripts/Event_On_Ray.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Event_On_Ray : MonoBehaviour, IRayInteractable
+{
+    [Header("Ray Events")]
+    public UnityEvent OnEnter;
+    public UnityEvent OnStay;
+    public UnityEvent OnExit;
+    public UnityEvent OnClick;
+
+    public void OnRayEnter() => OnEnter?.Invoke();
+    public void OnRayStay() => OnStay?.Invoke();
+    public void OnRayExit() => OnExit?.Invoke();
+    public void OnRayClick() => OnClick?.Invoke();
+}
