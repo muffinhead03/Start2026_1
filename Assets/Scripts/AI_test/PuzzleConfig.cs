8fb3c371052fb9642815c5ba152e78e79dd8604b
png401 <png401@gmail.com>
2026-05-25 18:20:06 +0900

    feat: connect ai_test & wine_scene

--- Assets/Scripts/AI_test/PuzzleConfig.cs
+++ Assets/Scripts/AI_test/PuzzleConfig.cs
@@ -21,21 +21,19 @@ public static class PuzzleConfigData
 {
     public static PuzzleConfig GetConfig(string puzzleId)
     {
-        if (puzzleId == "clock_bookshelf_lock")
+        if (puzzleId == "wine_glass_room")
         {
             return new PuzzleConfig
             {
                 puzzleId = puzzleId,
-                totalSteps = 6,
-                requiredClues = new List<string> { "book_numbers", "clock_time" },
+                totalSteps = 4,
+                requiredClues = new List<string> { "clue_B", "clue_C" },
                 steps = new List<PuzzleStep>
                 {
-                    new PuzzleStep { id = 1, goal = "서재에 들어간다",             hintDirection = "이동 유도" },
-                    new PuzzleStep { id = 2, goal = "책장을 조사한다",             hintDirection = "오브젝트 집중" },
-                    new PuzzleStep { id = 3, goal = "책등의 숫자를 발견한다",      hintDirection = "단서 발견" },
-                    new PuzzleStep { id = 4, goal = "시계를 조사한다",             hintDirection = "오브젝트 집중" },
-                    new PuzzleStep { id = 5, goal = "시계와 책장 숫자 관계 이해",  hintDirection = "추론 유도" },
-                    new PuzzleStep { id = 6, goal = "자물쇠에 정답 입력",          hintDirection = "행동 안내" },
+                    new PuzzleStep { id = 1, goal = "와인잔을 조사한다", hintDirection = "와인잔 색깔 확인" },
+                    new PuzzleStep { id = 2, goal = "와인랙 라벨을 조사한다", hintDirection = "색깔-알파벳 매칭" },
+                    new PuzzleStep { id = 3, goal = "교차점 숫자를 확인한다", hintDirection = "숫자 확인" },
+                    new PuzzleStep { id = 4, goal = "정답 가구를 조사한다", hintDirection = "SOFA 찾기" },
                 }
             };
         }
