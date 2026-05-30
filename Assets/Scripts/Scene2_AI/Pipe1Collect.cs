e9ac22334bdabdf2dabaf608656c723a79fa830e
png401 <png401@gmail.com>
2026-05-30 21:53:39 +0900

    feat: add organ scene phase1 inspect scripts
    - OrganManual, Pipe1~4Collect, Room2KeyCollect
    - Room2Door, Room3Keypad, LPHintCollect, LPPlayer, OrganRepair
    - each script notifies HintManager on interaction

---
+++ Assets/Scripts/Scene2_AI/Pipe1Collect.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Pipe1Collect : MonoBehaviour
+{
+    public HintManager hintManager;
+    bool collected = false;
+
+    public void Collect()
+    {
+        if (collected) return;
+        collected = true;
+        hintManager.currentPlayerState.foundClues.Add("clue_pipe_1");
+        Player_Inventory.AddItem("pipe_1");
+        gameObject.SetActive(false);
+        Debug.Log("[Pipe1] 파이프 1 수집");
+    }
+}
---
+++ Assets/Scripts/Scene2_AI/Pipe3Collect.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Pipe3Collect : MonoBehaviour
+{
+    public HintManager hintManager;
+    bool collected = false;
+
+    public void Collect()
+    {
+        if (collected) return;
+        collected = true;
+        hintManager.currentPlayerState.foundClues.Add("clue_pipe_3");
+        Player_Inventory.AddItem("pipe_3");
+        gameObject.SetActive(false);
+        Debug.Log("[Pipe3] 파이프 3 수집");
+    }
+}
