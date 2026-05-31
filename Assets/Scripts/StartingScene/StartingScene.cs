114c86ed92eb9ce1a07be41d2108309d3a16ed96
ilovedc10 <ilovedc10@naver.com>
2026-05-31 16:15:09 +0900

    인트로 수정

--- Assets/Scripts/StartingScene/StartingScene.cs
+++ Assets/Scripts/StartingScene/StartingScene.cs
@@ -1,20 +1,86 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartingScene : MonoBehaviour
 {
     [Header("Start Game")]
     [SerializeField] private string tutorialSceneName = "Tutorial";
-
-    [Header("Default Continue Scene")]
     [SerializeField] private int defaultSceneNumber = 1;
+
+    [Header("Intro Screen")]
+    [SerializeField] private Image introScreen;
+    [SerializeField] private float introDuration = 2f;
+    [SerializeField] private float introTargetScale = 1.3f;
 
     [Header("Setting Panel")]
     [SerializeField] private GameObject settingPanel;
 
+    private void Start()
+    {
+        if (introScreen != null)
+        {
+            Color color = introScreen.color;
+            color.a = 0f;
+            introScreen.color = color;
+
+            introScreen.rectTransform.localScale = Vector3.one;
+            introScreen.gameObject.SetActive(false);
+        }
+
+        if (settingPanel != null)
+        {
+            settingPanel.SetActive(false);
+        }
+    }
+    
     public void StartGame()
     {
         Debug.Log("Start Button Clicked");
+
+        StartCoroutine(ShowIntroScreenAndLoadScene());
+    }
+
+    private IEnumerator ShowIntroScreenAndLoadScene()
+    {
+        if (introScreen == null)
+        {
+            Debug.LogError("Intro Screen이 연결되지 않았습니다.");
+            SceneManager.LoadScene(tutorialSceneName);
+            yield break;
+        }
+
+        RectTransform introRect = introScreen.GetComponent<RectTransform>();
+
+        introScreen.gameObject.SetActive(true);
+
+        Color color = introScreen.color;
+        color.a = 0f;
+        introScreen.color = color;
+
+        introRect.localScale = Vector3.one;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < introDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float t = elapsedTime / introDuration;
+
+            color.a = Mathf.Lerp(0f, 1f, t);
+            introScreen.color = color;
+
+            float scale = Mathf.Lerp(1f, introTargetScale, t);
+            introRect.localScale = new Vector3(scale, scale, 1f);
+
+            yield return null;
+        }
+
+        color.a = 1f;
+        introScreen.color = color;
+        introRect.localScale = new Vector3(introTargetScale, introTargetScale, 1f);
 
         SceneManager.LoadScene(tutorialSceneName);
     }
