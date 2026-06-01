13d007cd4ec80798af369955383c01c3a5ccd0a9
png401 <png401@gmail.com>
2026-06-01 18:01:51 +0900

    feat: add 힌트ai 관련 스크립트 추가

--- Assets/Scripts/Object_Pwd.cs
+++ Assets/Scripts/Object_Pwd.cs
@@ -5,6 +5,7 @@ public class Object_Pwd : MonoBehaviour
 {
     public string pwd;
     public Object_Pwd_Locked obj;
+    public HintManager hintManager; // 추가
 
     private string input="";
 
@@ -13,7 +14,11 @@ public class Object_Pwd : MonoBehaviour
         Debug.Log(n);
         if (n == "submit")
         {
-            if (pwd == input) obj.UnlockDoor();
+            if (pwd == input) 
+            {
+                obj.UnlockDoor();
+                hintManager.currentPlayerState.completedSteps.Add(5);
+            }
             else input = "";
         }
         else input += n;
