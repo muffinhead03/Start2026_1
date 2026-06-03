04ab531cbafadace4ea9db2cb12de71dab49b698
mjyuung <yoonminjoo03@ewhain.net>
2026-06-03 14:33:21 +0900

    fix: 단서 조사, 무전기 사용 시 플레이어 이동 잠금

--- Assets/Scripts/AI_test/HintManager.cs
+++ Assets/Scripts/AI_test/HintManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class HintManager : MonoBehaviour
 {
+    [Header("Player Character")]
+    [SerializeField] EX_InputSystem_PC_V2 player;
+
     [Header("연결 필요")]
     [SerializeField] OllamaClient        ollamaClient;
     [SerializeField] GameObject          hintPanel;
@@ -72,11 +75,13 @@ public class HintManager : MonoBehaviour
             hintText.text = "치지직... 도움이 필요해?";
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible   = true;
+            player.SetMoveLock(true);
         }
         else
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible   = false;
+            player.SetMoveLock(false);
         }
     }
 
