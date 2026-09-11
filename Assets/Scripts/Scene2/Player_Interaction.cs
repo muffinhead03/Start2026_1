11af7d0d2af7ae5edaba5a0be77ddbc5658591fb
mjyuung <unminjoo033@gmail.com>
2026-09-11 23:53:22 +0900

    feat: 키 입력 안내 UI 추가
    - 모든 씬 Canvas와 Scene_UI_Manager에 적용
    - 상호작용 가능 시 E 활성화

--- Assets/Scripts/Scene2/Player_Interaction.cs
+++ Assets/Scripts/Scene2/Player_Interaction.cs
@@ -55,6 +55,7 @@ public class Player_Interaction : MonoBehaviour
                     CurrentTarget = Interactable;
                     CurrentTarget.OnRayEnter();
                     SceneUI.SwitchCursor(true);
+                    SceneUI.SetActivePanel(3, true);
                 }
 
                 CurrentTarget.OnRayStay();
@@ -68,6 +69,7 @@ public class Player_Interaction : MonoBehaviour
                     CurrentTarget.OnRayExit();
                     CurrentTarget = null;
                     SceneUI.SwitchCursor(false);
+                    SceneUI.SetActivePanel(3, false);
                 }
             }
         }
@@ -79,6 +81,7 @@ public class Player_Interaction : MonoBehaviour
                 CurrentTarget.OnRayExit();
                 CurrentTarget = null;
                 SceneUI.SwitchCursor(false);
+                SceneUI.SetActivePanel(3, false);
             }
         }
     }
