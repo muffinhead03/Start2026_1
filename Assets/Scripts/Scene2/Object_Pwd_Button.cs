e24cb22bd13ac44d6b794cf275f484e4406e88ff
mjyuung <yoonminjoo03@ewhain.net>
2026-09-17 18:04:11 +0900

    feat: 키패드 버튼, 액자, 사진, 악보방 가구 추가
    - 키패드 버튼 누를 때 버튼 빛나도록
    - 과일 그림 액자 프리팹 적용
    - 악보 방 가구 추가

--- Assets/Scripts/Scene2/Object_Pwd_Button.cs
+++ Assets/Scripts/Scene2/Object_Pwd_Button.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,5 +8,33 @@ public class Object_Pwd_Button : MonoBehaviour
     public string id;
     public UnityEvent OnPressed;
 
-    public void Pressed() => OnPressed?.Invoke();
+    [Header("Materials")]
+    [SerializeField] Material[] materials;
+
+    MeshRenderer mesh;
+
+    Collider col;
+
+    public void Pressed()
+    {
+        OnPressed?.Invoke();
+
+        StartCoroutine(ChangeMaterial());
+    }
+
+    IEnumerator ChangeMaterial()
+    {
+        if (mesh == null) mesh = GetComponent<MeshRenderer>();
+        if (col == null) col = GetComponent<Collider>();
+
+        mesh.material = materials[1];
+
+        col.enabled = false;
+
+        yield return new WaitForSeconds(0.5f);
+
+        mesh.material = materials[0];
+
+        col.enabled = true;
+    }
 }
